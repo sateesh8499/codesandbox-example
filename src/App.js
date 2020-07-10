5521ae65e7ca718633405689e4358f884a184782
sateesh8499 <46959587+sateesh8499@users.noreply.github.com>
2020-07-10 15:06:50 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Guys...</h1>
+      <h2>Welcome to our Website!</h2>
+    </div>
+  );
+}
